59ce0bc3b592da6ebee44db8631e01ddb77a9d6f
gchatain <gchatain@stduent.42lyon.fr>
2023-02-06 16:09:58 +0100

    Makefile complet, init main en commencement

---
+++ sources/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "ft_irc.h"
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+
+    } else {
+        std::cout << "usage ./ft_irc [port]" << std::endl;
+    }
+}
